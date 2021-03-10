90044fe8f1cffc55b7a276af0380a34aa6b51178
leopoldkwok <leopoldkwok@gmail.com>
2021-03-10 15:40:21 +0000

    Display Single Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import Follower from './Follower';
 
 function App() {
   const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading]);
 
   return (
     <main>
@@ -12,7 +19,7 @@ function App() {
         <div className='underline'></div>
         <section className='followers'>
           <div className='container'>
-            {data.map((follower) => {
+            {followers.map((follower) => {
               return <Follower key={follower.id} {...follower} />;
             })}
           </div>
